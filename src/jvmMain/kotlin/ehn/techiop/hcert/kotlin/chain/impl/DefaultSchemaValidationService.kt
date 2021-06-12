0290eb6dda3af0fa65fd82a34c2d308e55a8638c
Bernd Prünster <bernd.pruenster@iaik.tugraz.at>
2021-06-12 22:30:53 +0200

    JVM: Work around forced URI for JSON schema validation

--- src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
+++ src/jvmMain/kotlin/ehn/techiop/hcert/kotlin/chain/impl/DefaultSchemaValidationService.kt
@@ -1,12 +1,36 @@
 package ehn.techiop.hcert.kotlin.chain.impl
 
+import com.upokecenter.cbor.CBORObject
+import ehn.techiop.hcert.kotlin.chain.Error
 import ehn.techiop.hcert.kotlin.chain.SchemaValidationService
 import ehn.techiop.hcert.kotlin.chain.VerificationResult
+import net.pwall.json.schema.parser.Parser
+import java.io.ByteArrayOutputStream
+import java.net.URI
 
 actual class DefaultSchemaValidationService : SchemaValidationService {
 
     override fun validate(cbor: ByteArray, verificationResult: VerificationResult) {
-        //TODO Implement Schema validation on JVM
+        try {
+            val decoded = CBORObject.DecodeFromBytes(cbor)
+            val json = ByteArrayOutputStream().let {
+                decoded.WriteJSONTo(it)
+                it.toString()
+            }
+            val resource = javaClass.classLoader.getResourceAsStream("json/DCC.combined-schema.json")
+                ?: throw IllegalArgumentException("Schema not found")
+            val parser = Parser(uriResolver = { resource })
+            val schema = parser.parse(URI.create("dummy:///"))
+            val result = schema.validateBasic(json)
+            result.errors?.let {
+                if (it.isNotEmpty()) {
+                    //it.forEach { println("${it.error} - ${it.instanceLocation}") }
+                    throw Throwable("Data does not follow schema: ${result.errors}")
+                }
+            }
+        } catch (t: Throwable) {
+            verificationResult.error = Error.CBOR_DESERIALIZATION_FAILED
+        }
     }
 
 }
